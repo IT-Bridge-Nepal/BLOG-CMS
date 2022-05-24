ebeb7dea2992c6e5a6beff6747be8cb5aa5d5565
MrBinaya <binaya.sharma90@gmail.com>
2022-05-24 12:29:34 +0545

    admin panel redesign

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root","","cms");
-if(!$con)
-{
+$con = mysqli_connect("localhost", "binaya", "Password@1", "blog_cms");
+if (!$con) {
     die("not connected");
-
 }
-
-?>
