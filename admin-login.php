c78a8d58f3b46b8d361ac736dec8a2a637610af2
sagarshrestha-414 <saagardhoju414@gmail.com>
2022-05-26 13:40:36 +0545

    image

---
+++ admin-login.php
@@ -0,0 +1,28 @@
+<?php 
+include("includes/db.php"); 
+
+if (isset($_POST['submit'])) {
+    //echo "button clicked";
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $sql = "SELECT * from tbl_admin WHERE username='$username'AND password='$password'";
+
+    $res = mysqli_query($conn, $sql);
+
+    $count = mysqli_num_rows($res);
+    // var_dump($count);
+    // die();
+
+    if ($count==1) {
+        $_SESSION['login'] = "<div class='success'>login success</div>";
+        header('location:'.SITEURL.'admin/index.php');
+    }
+} else {
+    //button not clicked
+    $_SESSION['login'] = "<div class='error'>password or user name didinot matched</div>";
+    header('location:'.SITEURL.'admin.php');
+}
+
+
+?>
